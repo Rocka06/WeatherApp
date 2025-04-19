d4868df99ef590d23db58ca49dc5400597afe524
Kertesi Csongor <csongor@kertesi.hu>
2025-04-19 20:21:15 +0000

    Add country flag next to location text

--- src/UpdateData.js
+++ src/UpdateData.js
@@ -34,7 +34,12 @@ const iconOfWMO = (WMO) => {
 }
 
 const updateCurrentCard = async (cityName, coords) => {
-    document.getElementById("nowLocation").textContent = cityName;
+    const flagEmoji = localStorage.getItem("countryCode")
+        .toUpperCase()
+        .split('')
+        .map(char => String.fromCodePoint(127397 + char.charCodeAt(0)))
+        .join('');
+    document.getElementById("nowLocation").textContent = cityName + " " + flagEmoji;
     let date = new Date();
     document.getElementById("nowDate").textContent = `${date.getFullYear()}. ${monthNames[date.getMonth()]} ${date.getDate()}., ${dayNames[date.getUTCDay()]}`;
 
@@ -48,5 +53,5 @@ const updateCurrentCard = async (cityName, coords) => {
     let sunrise = new Date(data.daily.sunrise[0]);
     let sunset = new Date(data.daily.sunset[0]);
     document.getElementById("nowSunrise").textContent = `${sunrise.getHours()}:${sunrise.getMinutes()}`;
-    document.getElementById("nowSunset").textContent = `${sunset.getHours()}:${sunset.getMinutes()}`;     
+    document.getElementById("nowSunset").textContent = `${sunset.getHours()}:${sunset.getMinutes()}`;
 }
